a07d9e4a133ac490faf76f22962f8320575e2e71
Josh Faskowitz <faskowit@users.noreply.github.com>
2019-01-07 10:06:12 -0500

    output a cortical mask that keeps parcels confined
    ...to one hemisphere

--- src/maTT2_applyGCS.sh
+++ src/maTT2_applyGCS.sh
@@ -326,20 +326,53 @@ fi
 if [[ ! -e ${outputDir}/${subj}_cortical_mask.nii.gz ]]
 then
 
-    #cmd="${FSLDIR}/bin/fslmaths \
-    #        ${subjAparcAseg} \
-    #        -thr 1000 -bin \
-    #        ${outputDir}/${subj}_cortical_mask.nii.gz \
-    #        -odt int \
-    #    "
+    # let's get a lh and rh, get largest component of each
     cmd="${FREESURFER_HOME}/bin/mri_binarize \
             --i ${subjAparcAseg} \
-            --min 1000 --binval 1 \
-            --o ${outputDir}/${subj}_cortical_mask.nii.gz \
+            --min 1000 --max 1999 --binval 1 \
+            --o ${outputDir}/lh.tmp_cort_mask.nii.gz \
         "     
     echo $cmd #state the command
     log $cmd >> $OUT
     eval $cmd #execute the command
+    cmd="${FREESURFER_HOME}/bin/mri_extract_largest_CC \
+            -T 1 \
+            ${outputDir}/lh.tmp_cort_mask.nii.gz \
+            ${outputDir}/lh.tmp_cort_mask.nii.gz \
+        "     
+    echo $cmd #state the command
+    log $cmd >> $OUT
+    eval $cmd #execute the command
+
+    cmd="${FREESURFER_HOME}/bin/mri_binarize \
+            --i ${subjAparcAseg} \
+            --min 2000 --max 2999 --binval 1 \
+            --o ${outputDir}/rh.tmp_cort_mask.nii.gz \
+        "     
+    echo $cmd #state the command
+    log $cmd >> $OUT
+    eval $cmd #execute the command
+    cmd="${FREESURFER_HOME}/bin/mri_extract_largest_CC \
+            -T 1 \
+            ${outputDir}/rh.tmp_cort_mask.nii.gz \
+            ${outputDir}/rh.tmp_cort_mask.nii.gz \
+        "     
+    echo $cmd #state the command
+    log $cmd >> $OUT
+    eval $cmd #execute the command
+
+    # write out the combined cortical_mask
+    cmd="${FREESURFER_HOME}/bin/mris_calc \
+            -o ${outputDir}/${subj}_cortical_mask.nii.gz \
+            ${outputDir}/lh.tmp_cort_mask.nii.gz \
+            add ${outputDir}/rh.tmp_cort_mask.nii.gz \
+        "
+    echo $cmd
+    log $cmd >> $OUT
+    eval $cmd
+
+    # remove the tmp
+    ls ${outputDir}/?h.tmp_cort_mask.nii.gz && rm ${outputDir}/?h.tmp_cort_mask.nii.gz
 
 fi
 
